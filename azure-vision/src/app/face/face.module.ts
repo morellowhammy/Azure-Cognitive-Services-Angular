0063caf58288ab274c03af42031124bfff2dc6c2
Victor Escudero <morellowhammy@hotmaill.com>
2018-11-24 00:03:18 +0100

    Created Face module
    Signed-off-by: Victor Escudero <morellowhammy@hotmaill.com>

---
+++ azure-vision/src/app/face/face.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FaceRoutingModule } from './face-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FaceRoutingModule
+  ]
+})
+export class FaceModule { }
