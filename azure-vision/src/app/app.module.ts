27e10c1d54fc9fc3d24730f5f4da63e50a295e73
Victor Escudero <morellowhammy@hotmaill.com>
2018-11-24 00:04:26 +0100

    Created CustomVision module
    Signed-off-by: Victor Escudero <morellowhammy@hotmaill.com>

--- azure-vision/src/app/app.module.ts
+++ azure-vision/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FaceModule } from './face/face.module';
+import { CustomVisionModule } from './custom-vision/custom-vision.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FaceModule } from './face/face.module';
   ],
   imports: [
     BrowserModule,
-    FaceModule
+    FaceModule,
+    CustomVisionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ azure-vision/src/app/custom-vision/custom-vision.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomVisionRoutingModule } from './custom-vision-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomVisionRoutingModule
+  ]
+})
+export class CustomVisionModule { }
