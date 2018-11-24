f42014463d52ef3b883d2c489109b54d66090089
Victor Escudero <morellowhammy@hotmaill.com>
2018-11-24 17:23:27 +0100

    Created shoot-face component inside face module (ng g c face/shoot-face)

--- azure-vision/src/app/face/face.module.ts
+++ azure-vision/src/app/face/face.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FaceRoutingModule } from './face-routing.module';
+import { ShootFaceComponent } from './shoot-face/shoot-face.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ShootFaceComponent],
   imports: [
     CommonModule,
     FaceRoutingModule
---
+++ azure-vision/src/app/face/shoot-face/shoot-face.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoot-face',
+  templateUrl: './shoot-face.component.html',
+  styleUrls: ['./shoot-face.component.css']
+})
+export class ShootFaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
