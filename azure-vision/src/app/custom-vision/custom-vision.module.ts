533cee74016173b8dbe61b399f7ca597bc55469c
Victor Escudero <morellowhammy@hotmaill.com>
2018-11-24 17:25:41 +0100

    Created hotdog component inside custom-vision module ( ng g c custom-vision/hotdog )

--- azure-vision/src/app/custom-vision/custom-vision.module.ts
+++ azure-vision/src/app/custom-vision/custom-vision.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomVisionRoutingModule } from './custom-vision-routing.module';
+import { HotdogComponent } from './hotdog/hotdog.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HotdogComponent],
   imports: [
     CommonModule,
     CustomVisionRoutingModule
---
+++ azure-vision/src/app/custom-vision/hotdog/hotdog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hotdog',
+  templateUrl: './hotdog.component.html',
+  styleUrls: ['./hotdog.component.css']
+})
+export class HotdogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
