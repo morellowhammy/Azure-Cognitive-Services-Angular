d0bd9b44175162f1c7eb73966e321c3ace16234c
Victor Escudero <morellowhammy@hotmaill.com>
2018-11-25 00:57:29 +0100

    Created Camara component

--- azure-vision/src/app/face/face.module.ts
+++ azure-vision/src/app/face/face.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { FaceRoutingModule } from './face-routing.module';
 import { ShootFaceComponent } from './shoot-face/shoot-face.component';
+import { CamaraComponent } from './camara/camara.component';
 
 @NgModule({
-  declarations: [ShootFaceComponent],
+  declarations: [ShootFaceComponent, CamaraComponent],
   imports: [
     CommonModule,
     FaceRoutingModule
