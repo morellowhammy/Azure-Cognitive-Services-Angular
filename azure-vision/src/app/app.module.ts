6c9c500648b37110cb086983ad6f2439eee6c56e
Victor Escudero <morellowhammy@hotmaill.com>
2018-11-24 18:42:45 +0100

    Created Angular Material Nav Component: ng g @angular/material:material-nav --name nav

--- azure-vision/src/app/app.module.ts
+++ azure-vision/src/app/app.module.ts
@@ -6,17 +6,27 @@ import { FaceModule } from './face/face.module';
 import { CustomVisionModule } from './custom-vision/custom-vision.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FaceModule,
     CustomVisionModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
