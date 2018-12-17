134fff13f6fb701a1653e492afa4210243346b4e
Victor Escudero <morellowhammy@hotmaill.com>
2018-12-17 20:17:02 +0100

    Created logic for the hot dog recognition component

--- azure-vision/src/app/custom-vision/hotdog/hotdog.component.ts
+++ azure-vision/src/app/custom-vision/hotdog/hotdog.component.ts
@@ -1,4 +1,9 @@
+import { HotdogVisionService } from './../shared/hotdog-vision.service';
+import { FaceService } from './../../shared/face.service';
 import { Component, OnInit } from '@angular/core';
+import { Subject, Observable, of } from 'rxjs';
+import { WebcamImage } from 'ngx-webcam';
+import { ImageProcessorService } from 'src/app/shared/image-processor.service';
 
 @Component({
   selector: 'app-hotdog',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotdogComponent implements OnInit {
 
-  constructor() { }
+  private trigger: Subject<void> = new Subject<void>();
+
+  public webcamImage: WebcamImage = null;
+
+  public jsonResult: string = null;
+
+  constructor(
+    private hotdogVisionService: HotdogVisionService,
+    private imageProcessorService: ImageProcessorService) { }
 
   ngOnInit() {
   }
 
+  public triggerSnapshot(): void {
+    this.trigger.next();
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    this.webcamImage = webcamImage;
+
+    this.imageProcessorService.imageUrltoBlob(webcamImage.imageAsDataUrl).subscribe((image) => {
+      this.hotdogVisionService.recognizeHotDog(image).subscribe((response: Array<any>) => {
+        if (response.length) {
+          console.log(response);
+          this.jsonResult = JSON.stringify(response[0], undefined, 2);
+        } else {
+          this.jsonResult = 'NO HOT DOG!!';
+        }
+      });
+    });
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
 }
