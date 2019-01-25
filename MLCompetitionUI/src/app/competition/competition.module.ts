3e940ccaa678cdf3beb3c2ecaf4da8bb391b98eb
Victor Escudero <morellowhammy@hotmaill.com>
2019-01-25 11:00:03 +0100

    Created registry component in competition: ng g c competition/registry

--- MLCompetitionUI/src/app/competition/competition.module.ts
+++ MLCompetitionUI/src/app/competition/competition.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistryComponent } from './registry/registry.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [RegistryComponent],
   imports: [
     CommonModule
   ]
