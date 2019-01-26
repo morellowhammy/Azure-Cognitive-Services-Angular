ad01953583cdf0411f852d609e904beb3e3674de
Victor Escudero <morellowhammy@hotmaill.com>
2019-01-26 20:11:34 +0100

    Prepared communication between rankingComponent and competitionService

--- MLCompetitionUI/src/app/competition/ranking/ranking.component.ts
+++ MLCompetitionUI/src/app/competition/ranking/ranking.component.ts
@@ -1,19 +1,5 @@
 import { CompetitionService } from './../../shared/competition.service';
 import { Component, OnInit } from '@angular/core';
-import { IRankingRow } from 'src/app/shared/ranking-row.model';
-
-const ELEMENT_DATA: IRankingRow[] = [
-  {position: 1, name: 'Hydrogen', score: 1.0079, attempts: 3},
-  {position: 2, name: 'Helium', score: 4.0026, attempts: 2},
-  {position: 3, name: 'Lithium', score: 6.941, attempts: 1},
-  {position: 4, name: 'Beryllium', score: 9.0122, attempts: 4},
-  {position: 5, name: 'Boron', score: 10.811, attempts: 6},
-  {position: 6, name: 'Carbon', score: 12.0107, attempts: 1},
-  {position: 7, name: 'Nitrogen', score: 14.0067, attempts: 1},
-  {position: 8, name: 'Oxygen', score: 15.9994, attempts: 1},
-  {position: 9, name: 'Fluorine', score: 18.9984, attempts: 1},
-  {position: 10, name: 'Neon', score: 20.1797, attempts: 2},
-];
 
 @Component({
   selector: 'app-ranking',
@@ -23,13 +9,15 @@ const ELEMENT_DATA: IRankingRow[] = [
 export class RankingComponent implements OnInit {
 
   public displayedColumns: string[];
-  public dataSource;
+  public rankingRows;
 
   constructor(public competitionService: CompetitionService) { }
 
   ngOnInit() {
     this.displayedColumns = ['position', 'name', 'score', 'attempts'];
-    this.dataSource = ELEMENT_DATA;
+    this.competitionService.getRankingsList().subscribe( (rankingRows) => {
+      this.rankingRows = rankingRows;
+    });
   }
 
 }
--- MLCompetitionUI/src/app/shared/competition.service.ts
+++ MLCompetitionUI/src/app/shared/competition.service.ts
@@ -9,6 +9,19 @@ const config = {
   uriBase: 'https://172.22.195.238/v1'
 };
 
+const ELEMENT_DATA: IRankingRow[] = [
+  {position: 1, name: 'Antuan', score: 1.0079, attempts: 3},
+  {position: 2, name: 'Vinicius', score: 4.0026, attempts: 2},
+  {position: 3, name: 'Leo', score: 6.941, attempts: 1},
+  {position: 4, name: 'Luis', score: 9.0122, attempts: 4},
+  {position: 5, name: 'Cristhian', score: 10.811, attempts: 6},
+  {position: 6, name: 'Iago', score: 12.0107, attempts: 1},
+  {position: 7, name: 'Ben', score: 14.0067, attempts: 1},
+  {position: 8, name: 'Roger', score: 15.9994, attempts: 1},
+  {position: 9, name: 'Maxi', score: 18.9984, attempts: 1},
+  {position: 10, name: 'Borja', score: 20.1797, attempts: 2},
+];
+
 @Injectable({
   providedIn: 'root'
 })
@@ -34,8 +47,10 @@ export class CompetitionService {
   public getRankingsList(): Observable<IRankingRow[]> {
     this.url = config.uriBase + '/ranking';
 
-    return this.http.get<IRankingRow[]>(this.url)
-      .pipe(catchError(this.handleError<IRankingRow[]>('getRankingsList', [])));
+    return of(ELEMENT_DATA as IRankingRow[]);
+
+    /*return this.http.get<IRankingRow[]>(this.url)
+      .pipe(catchError(this.handleError<IRankingRow[]>('getRankingsList', [])));*/
 
   }
 
