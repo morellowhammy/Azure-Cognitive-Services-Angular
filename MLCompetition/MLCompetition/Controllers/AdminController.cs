bedc3be249f1feb847988149d477a74cc85dbba1
Victor Escudero <morellowhammy@hotmaill.com>
2019-02-13 09:37:03 +0100

    NetCoreApì: Created Admin Controller

---
+++ MLCompetition/MLCompetition/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MLCompetition.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private bool _competitionEnabled = true;
+
+        [HttpGet]
+        [Route("competition-enable")]
+        public ActionResult<string> GetCompetitionEnabled()
+        {
+            return Ok(_competitionEnabled);
+        }
+
+        [HttpPost]
+        [Route("competition-enable")]
+        public ActionResult Post([FromBody] bool competitionEnable)
+        {
+            this._competitionEnabled = competitionEnable;
+            return Ok(_competitionEnabled);
+        }
+    }
+}
