8d51781ce181400bc502bcf5c5af65a0c7ccd4b8
Victor Escudero <morellowhammy@hotmaill.com>
2019-02-13 17:02:34 +0100

    Restrict mlstudio exercise info until the competition is started.

--- MLCompetitionUI/src/app/home/mlstudio-exercise/mlstudio-exercise.component.ts
+++ MLCompetitionUI/src/app/home/mlstudio-exercise/mlstudio-exercise.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/admin/shared/admin.service';
 
 @Component({
   selector: 'app-mlstudio-exercise',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MlstudioExerciseComponent implements OnInit {
 
-  constructor() { }
+  public isCompetitionStarted = false;
+
+  constructor(public adminService: AdminService) { }
 
   ngOnInit() {
+    this.adminService.getCompetitionEnable().subscribe( (response) => {
+      this.isCompetitionStarted = (response === 'true');
+    });
   }
 
 }
