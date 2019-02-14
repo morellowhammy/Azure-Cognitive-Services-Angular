967dae1711c850749f3ab3d5cfeab370df9ac11f
Victor Escudero <morellowhammy@hotmaill.com>
2019-02-14 18:04:47 +0100

    NetCoreApi: Create a new service to setup the number of scoring tests

--- MLCompetition/MLCompetition/Interfaces/IScoreService.cs
+++ MLCompetition/MLCompetition/Interfaces/IScoreService.cs
@@ -5,6 +5,7 @@ namespace MLCompetition.Interfaces
 {
     public interface IScoreService
     {
+        int NumberOfTests { get; set; }
         Task<double> CalculateScoreAsync(string apiAccessToken, string endpoint);
         Task<double> CalculateScoreConcurrent(string apiAccessToken, string endpoint);
         Task<IEnumerable<string>> ValidateEndpoint(string apiAccessToken, string endpoint);
